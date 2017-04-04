fdcbc431cf653a1f9ef37340f9b43cacbc47071b
luokerenz <luokerenz@gmail.com>
2017-04-04 18:11:02 -0400

    update search bar

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ import pandas as pd
 import time
 from ConfigParser import SafeConfigParser
 
-global mysqlConfig
 mysqlConfig = SafeConfigParser()
-mysqlConfig.read('/home/flask/mysqlConfig.ini')
+mysqlConfig.read('mysqlConfig.ini')
 
 """
 A example for creating a Table that is sortable by its header
@@ -16,7 +15,7 @@ A example for creating a Table that is sortable by its header
 
 app = Flask(__name__, static_url_path = "", static_folder="static")
 # init dictionary lookup
-conv_dict = pd.read_csv('/home/flask/bokeh_ref.csv',index_col='id').drop('internal_name',1).to_dict()
+conv_dict = pd.read_csv('bokeh_ref.csv',index_col='id').drop('internal_name',1).to_dict()
 global id_to_name
 id_to_name = conv_dict['name']
 
@@ -38,15 +37,22 @@ def indexQ():
     mhost = mysqlConfig.get('all', 'host')
     muser = mysqlConfig.get('all', 'user')
     mpass = mysqlConfig.get('all', 'password')
-    mainQ = mysqlConfig.get('all', 'mainQ')
     conn = MySQLdb.connect(host=mhost,user=muser,passwd=mpass,db='dota',charset='utf8',use_unicode=True)
     cur = conn.cursor()
-    qStr = mainQ + """ > \"%s\" limit 100""" %request_time
-    cur.execute(qStr)
-    conn.commit()
-    returnQ = cur.fetchall()
-    conn.close()
-    cur.close()
+
+    attempts = 0
+    while attempts < 3:
+        try:
+            qStr = """select match_id,player0Hero,player1Hero,player2Hero,player3Hero,player4Hero,player5Hero,player6Hero,player7Hero,player8Hero,player9Hero from Nmatch_history where replay_url is not null and parser_status=3 and start_time > \"%s\" limit 100""" %request_time
+            cur.execute(qStr)
+            returnQ = cur.fetchall()
+            conn.close()
+            cur.close()
+            break
+        except MySQLdb.Error, e:
+            attempts += 1
+            print "MySQL Error %d: %s" % (e.args[0], e.args[1])
+
     t1=time.time()
     #print 'db query cost: %s' %(t1-t0)
     for x in returnQ:
@@ -72,5 +78,10 @@ def index():
     formated_data = indexQ()
     return render_template("index.html", table=formated_data)
 
+@app.route('/match')
+def temp_bokeh():
+    return redirect('http://192.168.1.149:5006')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+	#print jdata
+  app.run(host='0.0.0.0', debug=True)
