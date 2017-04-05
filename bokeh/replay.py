96e75c6d249717687ad6080a9e36d726ad821671
luokerenz <luokerenz@gmail.com>
2017-04-05 00:03:35 -0400

    many new features

--- bokeh/replay.py
+++ bokeh/replay.py
@@ -8,7 +8,7 @@ import json
 from bokeh.io import curdoc
 from bokeh.layouts import row, column, widgetbox
 from bokeh.models import LinearAxis, ColumnDataSource, HoverTool
-from bokeh.models.widgets import Slider, Toggle, CheckboxButtonGroup
+from bokeh.models.widgets import Slider, Toggle, CheckboxButtonGroup, DataTable, TableColumn, Panel, Tabs, Div
 from bokeh.models.glyphs import ImageURL, Segment
 from bokeh.plotting import figure
 from bokeh.models.callbacks import CustomJS
@@ -17,6 +17,8 @@ from ConfigParser import SafeConfigParser
 global mongoConfig
 mongoConfig = SafeConfigParser()
 mongoConfig.read('/home/bokeh/mongoConfig.ini')
+
+styleCSS = """"""
 
 def timeConverter(seconds):
     m, s = divmod(int(seconds), 60)
@@ -33,11 +35,14 @@ def icon_finder(hero_id, id_to_name): #input: hero_id, dictionary used for id to
 
 # Set up callbacks
 def update_plot(attrname, old, new):
+    global ahl
     tick = int(time_slider.value)
     time_slider.title = timeConverter(tick)
     cd = col.find_one({"tag": 1, "matchid": int(match_id), "time":int(tick)})["data"]
     cgd = [int(0)]*10
     cexp = [int(0)]*10
+    cdmg_h = [int(0)]*10
+    cdmg_l = [int(0)]*10
     ahl = [0]*10
     circleX = []
     circleY = []
@@ -52,39 +57,20 @@ def update_plot(attrname, old, new):
         circleY.extend([cd[i]["d"][1]])
         cgd[i] = cd[i]["d"][2]
         cexp[i] = cd[i]["d"][3]
+        cdmg_h[i] = cd[i]["d"][7]
+        cdmg_l[i] = cd[i]["d"][8]
 
     max_y = max(cgd+cexp)+200
-    #print p2.y_range.start
-    #print p2.y_range.end
 
     source.data = columnDict
     circle_source.data['x'] = circleX
     circle_source.data['y'] = circleY
     g_source.data['height'] = cgd
     exp_source.data['height'] = cexp
-    #g_source.data = dict(x=[ind+0.25 for ind in range(1,20,2)], height=cgd, y=[0]*10, color=["FF7F0E"]*10)
-    #exp_source.data = dict(x=[ind-0.25 for ind in range(2,21,2)], height=cexp, y=[0]*10, color=["1F77B4"]*10)
-    #hero_img_source.data = dict(url=heroURL_list, x=[ind+0.5 for ind in range(1,20,2)], y=[max_y*0.1]*10, h=[1/(21/max_y)]*10)
+    dmgH_source.data['height'] = cdmg_h
+    dmgL_source.data['height'] = cdmg_l
 
-    if 0 in analysis_grp.active:
-        #update gold analysis
-        floor_tick = int(tick/60)*60
-        pre_tick = segdata_3.data['tickCheck'][0]
-        # if new tick is out of range of previous tick, update
-        if abs(pre_tick-floor_tick)>=60:
-            current_y = segdata_3.data['y0']
-            print 'looking for time: %s' %floor_tick
-            anad = db.aggreResult.find({"$and": [{"_id.time":floor_tick},{"$or": [{"_id.h":ahl[0]},{"_id.h":ahl[1]},{"_id.h":ahl[2]},{"_id.h":ahl[3]},{"_id.h":ahl[4]},{"_id.h":ahl[5]},{"_id.h":ahl[6]},{"_id.h":ahl[7]},{"_id.h":ahl[8]},{"_id.h":ahl[9]},]},{"_id.avg_mmrk": int(3)}]})
-            for inda in anad:
-                update_playerID = ahl.index(int(inda['_id']['h']))
-                current_y[int(update_playerID)] = inda['g_avg']
-    #segdata_3.data = dict(x0=[ind for ind in range(1,20,2)],y0=current_y,x1=[ind+0.5 for ind in range(1,20,2)],y1=current_y, alphacc=current_alpha)
-            segdata_3.data['y0'] = current_y
-            segdata_3.data['y1'] = current_y
-            segdata_3.data['alphacc'] = [0]*10
-            segdata_3.data['alphacc'] = [1]*10
-            segdata_3.data['tickCheck'] = [floor_tick]*10
-
+    updateBracket('data',{'value':0},{'value':0})
     hero_img_source.data = dict(url=heroURL_list, x=[ind+0.5 for ind in range(1,20,2)], y=[max_y*0.1]*10, h=[1/(21/max_y)]*10)
 
 def toggle_cc(arg):
@@ -99,15 +85,48 @@ def constant_update():
 
 def analysis_update(new):
     switch = analysis_grp.active
-    for x in range(0,len(analysis_list)):
-        if x in switch:
-            segdata_3.data['alphacc']=[1]*10
+    if 0 in switch: # switch for bracket data
+        segdata_3.data['alphacc']=[1]*20
+    else:
+        segdata_3.data['alphacc']=[0]*20
+    updateBracket('data',{'value':0},{'value':1})
+
+def updateBracket(attrname, old, new):
+    global ahl
+    if 0 in analysis_grp.active:
+        #update gold analysis
+        tick = int(time_slider.value)
+        floor_tick = int(tick/60)*60
+        if old['value']==new['value']:
+            pre_tick = segdata_3.data['tickCheck'][0]
         else:
-            segdata_3.data['alphacc']=[0]*10
+            pre_tick = 70 # bypass to force update
+        # if new tick is out of range of previous tick, update
+        if abs(pre_tick-floor_tick)>=60:
+            current_y = segdata_3.data['y0']
+            dmg_currentY = segdata_3.data['y0_dmg']
+            #print 'looking for time: %s' %floor_tick
+            anad = db.aggreResult.find({"$and": [{"_id.time":floor_tick},{"$or": [{"_id.h":ahl[0]},{"_id.h":ahl[1]},{"_id.h":ahl[2]},{"_id.h":ahl[3]},{"_id.h":ahl[4]},{"_id.h":ahl[5]},{"_id.h":ahl[6]},{"_id.h":ahl[7]},{"_id.h":ahl[8]},{"_id.h":ahl[9]},]},{"_id.avg_mmrk": int(bracket_input.value)}]})
+            for inda in anad:
+                update_playerID = ahl.index(int(inda['_id']['h']))
+                current_y[int(update_playerID)*2] = inda['g_avg']
+                current_y[int(update_playerID)*2+1] = inda['exp_avg']
+                dmg_currentY[int(update_playerID)*2] = inda['dmgH_avg']
+                #dmg_currentY[int(update_playerID)*2+1] = inda['exp_avg']
+
+            segdata_3.data['y0'] = current_y
+            segdata_3.data['y1'] = current_y
+            segdata_3.data['y0_dmg'] = dmg_currentY
+            segdata_3.data['y1_dmg'] = dmg_currentY
+            segdata_3.data['alphacc'] = [0]*20
+            segdata_3.data['alphacc'] = [1]*20
+            segdata_3.data['tickCheck'] = [floor_tick]*20
 
 # init global
 global play_state, match_id
+global ahl # global hero name list
 play_state = False
+ahl = [0]*10
 args = curdoc().session_context.request.arguments
 
 try:
@@ -130,7 +149,7 @@ global col
 col = db.zoneResult
 
 # init dictionary lookup
-conv_dict = pd.read_csv('/home/bokeh/bokeh_ref.csv',index_col='id').drop('internal_name',1).to_dict()
+conv_dict = pd.read_csv('bokeh_ref.csv',index_col='id').drop('internal_name',1).to_dict()
 id_to_name = conv_dict['name']
 name_to_id = {v: y for y, v in id_to_name.iteritems()}
 
@@ -143,10 +162,14 @@ for doc in cur:
     end_time = doc['time']
 
 # init dataFrame
-cd = col.find_one({"tag": 1, "matchid": int(match_id), "time":int(start_time)})["data"]
+cd_parent = col.find_one({"tag": 1, "matchid": int(match_id), "time":int(start_time)})
+cavg_mmr = int(cd_parent['avg_mmr'])
+cd = cd_parent["data"]
 name_list = [' ']*10
 gd = [int(0)]*10
 exp = [int(0)]*10
+dmg_h = [int(0)]*10
+dmg_l = [int(0)]*10
 heroURL_list = [""]*10
 circleX = []
 circleY = []
@@ -162,6 +185,8 @@ for i in range(10):
     circleY.extend([cd[i]["d"][1]])
     gd[i] = cd[i]["d"][2]
     exp[i] = cd[i]["d"][3]
+    dmg_h[i] = cd[i]["d"][7]
+    dmg_l[i] = cd[i]["d"][8]
     name_list[i] = id_to_name[int(player_hero)].replace('_', ' ')
 
 source = ColumnDataSource(data=columnDict)
@@ -169,10 +194,15 @@ circle_source = ColumnDataSource(data=dict(x=circleX,y=circleY,color=["#95cc4b",
 
 g_source = ColumnDataSource(data=dict(x=[ind+0.25 for ind in range(1,20,2)], height=gd, y=[0]*10, hero=name_list))
 exp_source = ColumnDataSource(data=dict(x=[ind-0.25 for ind in range(2,21,2)], height=exp, y=[0]*10, hero=name_list))
+dmgH_source = ColumnDataSource(data=dict(x=[ind+0.25 for ind in range(1,20,2)], height=dmg_h, y=[0]*10, hero=name_list))
+dmgL_source = ColumnDataSource(data=dict(x=[ind-0.25 for ind in range(2,21,2)], height=dmg_l, y=[0]*10, hero=name_list))
 img_source = ColumnDataSource(data=dict(url=[url_map],x=[0],y=[0],w=[32768],h=[32768]))
 hero_img_source = ColumnDataSource(data=dict(url=heroURL_list, x=[ind+0.5 for ind in range(1,20,2)], y=[200]*10, h=[40]*10))
-segdata_3 = ColumnDataSource(data=dict(x0=[ind for ind in range(1,20,2)],y0=[0]*10,x1=[ind+0.5 for ind in range(1,20,2)],y1=[0]*10,alphacc=[0]*10,tickCheck=[0]*10))
+segx0 = [1,1.5,3,3.5,5,5.5,7,7.5,9,9.5,11,11.5,13,13.5,15,15.5,17,17.5,19,19.5]
+segdata_3 = ColumnDataSource(data=dict(x0=segx0,y0=[0]*20,x1=[ind+0.5 for ind in segx0],y1=[0]*20,alphacc=[0]*20,tickCheck=[0]*20,y0_dmg=[0]*20,y1_dmg=[0]*20))
+info_source = ColumnDataSource(data=dict(mmr=[cavg_mmr]))
 minimap = ImageURL(url="url",x="x", y="y", w="w", h="h", anchor="center", global_alpha=0.7)
+divStr = styleCSS+"""<a href=https://www.opendota.com/matches/"""+str(match_id)+""">OpenDota Link</a>"""
 
 # Set up plot
 p = figure(plot_height=600, plot_width=600, title="", toolbar_location=None)
@@ -191,41 +221,65 @@ p.image_url(url="player8", x="x8", y="y8", w=1500, h=1500, anchor="center", glob
 p.image_url(url="player9", x="x9", y="y9", w=1500, h=1500, anchor="center", global_alpha=1, source=source)
 p.x_range.start = p.y_range.start = -18000
 p.y_range.end = p.x_range.end = 18000
+ptab = Panel(child=p, title="Minimap view")
+
+map_tabs = Tabs(tabs=[ ptab ],width=600)
 
 p2Hover = HoverTool(tooltips=[('Hero','@hero'),('Value','@height')])
 p2 = figure(plot_height=600, plot_width=600, title="", toolbar_location=None, tools=[p2Hover])
 p2.vbar(x="x", width=0.5, top="height", source=g_source, fill_color="#ff7f0e", line_color="#ff7f0e", legend="Gold")
 p2.vbar(x="x", width=0.5, top="height", source=exp_source, fill_color="#1f77b4", line_color="#1f77b4", legend="Experience")
-segment_3 = p2.segment(x0="x0", y0="y0", x1="x1", y1="y1", source=segdata_3, line_alpha='alphacc', line_color="#ff0000", line_width=10, legend="Hero Average")
-analysis_list = [segment_3]
+p2.segment(x0="x0", y0="y0", x1="x1", y1="y1", source=segdata_3, line_alpha='alphacc', line_color="#ff0000", line_width=10, legend="Bracket Average")
 p2.image_url(url="url", x="x", y="y", w=1, h="h", anchor="center", source=hero_img_source)
 p2.y_range.start = 0
+p2tab = Panel(child=p2, title="Gold/Experience")
 
-#p2.y_range.end = 1000
-#p2.y_range = Range1d(0, 20000)
+p3Hover = HoverTool(tooltips=[('Hero','@hero'),('Value','@height')])
+p3 = figure(plot_height=600, plot_width=600, title="", toolbar_location=None, tools=[p3Hover])
+p3.vbar(x="x", width=0.5, top="height", source=dmgH_source, fill_color="#ff7f0e", line_color="#ff7f0e", legend="Hero damage")
+p3.vbar(x="x", width=0.5, top="height", source=dmgL_source, fill_color="#1f77b4", line_color="#1f77b4", legend="Lane creep damage")
+p3.segment(x0="x0", y0="y0_dmg", x1="x1", y1="y1_dmg", source=segdata_3, line_alpha='alphacc', line_color="#ff0000", line_width=10, legend="Bracket Average")
+p3.image_url(url="url", x="x", y="y", w=1, h="h", anchor="center", source=hero_img_source)
+p3.y_range.start = 0
+p3tab = Panel(child=p3, title="Cumulated Damage")
+
+data_tabs = Tabs(tabs=[ p2tab, p3tab ],width=600)
 
 # Set up widgets
-time_slider = Slider(title="time", value=int(start_time), start=int(start_time), end=int(end_time), step=1, callback_policy="mouseup")
+time_slider = Slider(title='Game time (s)', value=int(start_time), start=int(start_time), end=int(end_time), step=1, callback_policy="mouseup")
 play_but = Toggle(label="Play", active=False)
-analysis_grp = CheckboxButtonGroup(labels=['Gold rank'])
+analysis_grp = CheckboxButtonGroup(labels=['Bracket data'])
+bracket_input = Slider(title='Ranking bracket (k)', value=3, start=0, end=9, step=1, callback_policy="mouseup")
+link_html = Div(text=divStr)
+info_columns = [
+        TableColumn(field="mmr", title="average mmr",sortable=False)
+    ]
+info_table = DataTable(source=info_source, columns=info_columns,height=100,fit_columns=True,selectable=False,sortable=False,row_headers=False)
 
-######
+###### custom intermediate trigger for slider mouseup ######
 interStrigger = ColumnDataSource(data=dict(value=[]))
 interStrigger.on_change('data', update_plot)
 time_slider.callback = CustomJS(args=dict(source=interStrigger), code="""
     source.data = { value: [cb_obj.value] }
 """)
+bracketTrigger = ColumnDataSource(data=dict(value=[]))
+bracketTrigger.on_change('data', updateBracket)
+bracket_input.callback = CustomJS(args=dict(source=bracketTrigger), code="""
+    source.data = { value: [cb_obj.value] }
+""")
 #####
 
 # widgets action link
-#time_slider.on_change('value', update_plot)
 play_but.on_click(toggle_cc)
 analysis_grp.on_click(analysis_update)
 
 curdoc().add_periodic_callback(constant_update, 500)
 
 # Set up layouts and add to document
-inputs = widgetbox(time_slider,play_but,analysis_grp)
+time_widget = widgetbox(time_slider,play_but)
+bracket_widget = widgetbox(bracket_input,analysis_grp)
+info_widget = widgetbox(link_html,info_table)
+#link_widget = widgetbox(link_html)
 
-curdoc().add_root(column(inputs, row(p, p2, width=1400)))
+curdoc().add_root(column(row(map_tabs, data_tabs,width=1400),row(time_widget,bracket_widget,info_widget)))
 curdoc().title = "Sliders"
